51b72e71c76316cde0177528857e07830144ee89
Whitman Huntley <huntleywhitman@gmail.com>
2016-01-05 09:00:20 -0500

    Documented everything
    Because. Also I can't spell. I know. shaddup.

--- src/development/modules/TimeID/module.go
+++ src/development/modules/TimeID/module.go
@@ -9,8 +9,12 @@ const Version string = "\033[1;35mV S-1.0.0"
 
 func GenerateID() string {
 	t := time.Now()
+	//set variable 't' as Time's "now" function
 	year, month, day := t.Date()
+	//Parces year, month and day from "t" using the date function
 	formatMonth := "SET"
+	//Declares a filler variable, nothing to see here.
+	//below checks the month and sets formatMonth to it's abbreviation.
 	switch month {
 	case time.January:
 		formatMonth = "JAN"
@@ -40,13 +44,14 @@ func GenerateID() string {
 		formatMonth = "NIL"
 	}
 	hour, min, _ := t.Clock()
-
+	//Parces hour, minuite from "t" using the clock function
 	strday := strconv.Itoa(day)
 	stryear := strconv.Itoa(year)
 	strhour := strconv.Itoa(hour)
 	strmin := strconv.Itoa(min)
+	//converts all of the above to strings for formatting
 
 	uuid := strday + "-" + formatMonth + "-" + stryear + "~" + strhour + ":" + strmin
-
+	//format's all of the variables into a UUID var and returns it
 	return uuid
 }
